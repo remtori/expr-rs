1876f610ea49310fa9a6b14f37b1ccbb01d90acd
Quang Vu <vulq2@vng.com.vn>
2024-10-31 15:34:26 +0700

    rt: opt_pass: remove silly long code

--- src/rt/opt_pass.rs
+++ src/rt/opt_pass.rs
@@ -15,14 +15,8 @@ fn constant_folding(ix_stream: &mut Vec<Instruction>) {
     for i in 0..ix_stream.len() {
         if i + 3 <= ix_stream.len() {
             match &ix_stream[i..i + 3] {
-                [Instruction::PushLit(a), Instruction::PushLit(b), op] => {
-                    let a = *a;
-                    let b = *b;
-                    let ret = match op {
-                        Instruction::BinaryOp(op) => Value::do_binary_op(a, b, *op),
-                        _ => return,
-                    };
-
+                [Instruction::PushLit(a), Instruction::PushLit(b), Instruction::BinaryOp(op)] => {
+                    let ret = Value::do_binary_op(*a, *b, *op);
                     ix_stream[i] = Instruction::PushLit(ret);
                     ix_stream.drain(i + 1..i + 3);
                 }
